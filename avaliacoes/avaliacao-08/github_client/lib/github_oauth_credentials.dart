8b869e482ef6e22179037a7fdc2cdd947ee8abf2
Iuri <yuriaragao160@gmail.com>
2026-05-27 20:21:28 -0300

    avaliação 8 feita

---
+++ avaliacoes/avaliacao-08/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liLn8ZKyAf3MjjZX';
+const githubClientSecret = 'a1fa7a5d11b9d2f32b368e7e109b9d109678cf06';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
